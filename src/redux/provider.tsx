a8d53b07cb5b94c82e26d9fc5935aa2cb252b98c
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-07-29 17:34:07 +0530

    update readme

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import store, { persistor } from "./store";
 import { Provider } from "react-redux";
-import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import authReducer from "./slice/auth.slice";
 import taskReducer from "./slice/task.slice";
 import toggleReducer from "./slice/toggle.slice";
 import userReducer from "./slice/user.slice";
 import taskStatusReducer from "./slice/taskStatus.slice";
 
+const persistConfig: PersistConfig<any> = {
+  key: "user",
+  storage,
+  whitelist: ["name"],
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    user: userReducer,
+    user: persistedUserReducer,
     task: taskReducer,
     toggle: toggleReducer,
     taskStatus: taskStatusReducer,
   },
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
